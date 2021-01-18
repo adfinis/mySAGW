2ed05fb0882c178591d3212200b27ec664d5fd3f
Fabio Ambauen <fabio@dnmx.ch>
2021-01-18 10:37:16 +0100

    feat: add "/me" retrieve & update views

--- api/mysagw/identity/serializers.py
+++ api/mysagw/identity/serializers.py
@@ -1,3 +1,5 @@
+from rest_framework_json_api import serializers
+
 from ..serializers import TrackingSerializer
 from . import models
 
@@ -6,3 +8,12 @@ class IdentitySerializer(TrackingSerializer):
     class Meta:
         model = models.Identity
         fields = TrackingSerializer.Meta.fields + ("idp_id", "first_name", "last_name",)
+
+
+class MeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Identity
+        fields = (
+            "first_name",
+            "last_name",
+        )
--- api/mysagw/identity/views.py
+++ api/mysagw/identity/views.py
@@ -1,3 +1,5 @@
+from rest_framework.mixins import RetrieveModelMixin, UpdateModelMixin
+from rest_framework.viewsets import GenericViewSet
 from rest_framework_json_api import views
 
 from . import models, serializers
@@ -8,3 +10,16 @@ class IdentityViewSet(views.ModelViewSet):
     serializer_class = serializers.IdentitySerializer
     queryset = models.Identity.objects.all()
     permission_classes = (IsAuthenticated & (IsAdmin | IsStaff),)
+
+
+class MeViewSet(
+    RetrieveModelMixin, UpdateModelMixin, GenericViewSet,
+):
+    serializer_class = serializers.MeSerializer
+
+    def get_object(self, *args, **kwargs):
+        return self.request.user.identity
+
+    def update(self, request, *args, **kwargs):
+        self.kwargs["pk"] = self.request.user.identity.pk
+        return super().update(request, *args, **kwargs)
