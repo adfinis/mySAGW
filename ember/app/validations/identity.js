ad1f8d51303ebf1c288419e87753810c76f8bc95
Florian Mäder <florian.maeder@adfinis-sygroup.ch>
2021-03-17 17:13:57 +0100

    feat(ember): change identity first and last name to optional

--- ember/app/validations/identity.js
+++ ember/app/validations/identity.js
@@ -4,8 +4,8 @@ import {
 } from "ember-changeset-validations/validators";
 
 export default {
-  firstName: [validatePresence(true)],
-  lastName: [validatePresence(true)],
+  firstName: [],
+  lastName: [],
   email: [validateFormat({ type: "email" })],
   organisationName: [
     validatePresence({ presence: true, on: "isOrganisation" }),
