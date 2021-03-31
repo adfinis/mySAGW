e08e08450a98a991adfdd14cde125b0386a55329
Florian Mäder <florian.maeder@adfinis-sygroup.ch>
2021-03-31 13:48:03 +0200

    feat(ember): add export to identity search/list.
    Export the current search as XLS.

--- ember/app/ui/identities/index/controller.js
+++ ember/app/ui/identities/index/controller.js
@@ -3,11 +3,18 @@ import { action } from "@ember/object";
 import { inject as service } from "@ember/service";
 import { tracked } from "@glimmer/tracking";
 import { timeout } from "ember-concurrency";
-import { restartableTask, lastValue } from "ember-concurrency-decorators";
+import {
+  restartableTask,
+  lastValue,
+  dropTask,
+} from "ember-concurrency-decorators";
+import { saveAs } from "file-saver";
+import moment from "moment";
 
 export default class IdentitiesIndexController extends Controller {
   @service notification;
   @service store;
+  @service intl;
 
   queryParams = ["pageSize", "pageNumber"];
   @tracked pageSize = 10;
@@ -52,5 +59,40 @@ export default class IdentitiesIndexController extends Controller {
 
   @action onUpdate() {
     this.fetchIdentities.perform();
+  }
+
+  @dropTask *exportSearch() {
+    const adapter = this.store.adapterFor("identity");
+
+    let uri = `${this.store
+      .adapterFor("identity")
+      .buildURL("identity")}/export`;
+
+    if (this.searchTerm) {
+      uri += `?filter[search]=${this.searchTerm}`;
+    }
+
+    const init = {
+      method: "POST",
+      headers: adapter.headers,
+    };
+
+    try {
+      const response = yield fetch(uri, init);
+
+      if (!response.ok) {
+        throw new Error(response.statusText || response.status);
+      }
+
+      const blob = yield response.blob();
+      const filename = `${this.intl.t("page.identities.index.export.filename", {
+        date: moment().format("YYYY-MM-DD"),
+      })}.xls`;
+
+      saveAs(blob, filename);
+    } catch (error) {
+      console.error(error);
+      this.notification.fromError(error);
+    }
   }
 }
