2375efb5fab43753a914bffa4843e98f32630628
Jonas Cosandey <jonas.cosandey@adfinis.com>
2021-04-01 11:07:23 +0200

    fix(memberships): dont show memberships if model.isOrganisation is set

--- ember/app/ui/identities/edit/controller.js
+++ ember/app/ui/identities/edit/controller.js
@@ -3,7 +3,13 @@ import { action } from "@ember/object";
 import { tracked } from "@glimmer/tracking";
 
 export default class IdentitiesEditController extends Controller {
-  @tracked isOrganisation = false;
+  @tracked _isOrganistaion;
+  get isOrganisation() {
+    return this._isOrganistaion ?? this.model.isOrganisation;
+  }
+  set isOrganisation(value) {
+    this._isOrganistaion = value;
+  }
 
   @action onOrganisationUpdate(isOrganisation) {
     this.isOrganisation = isOrganisation;
