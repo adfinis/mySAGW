f1238a8c28215710b3a3196067574b2efa2d5fd0
yelinz <yelin.zhang@adfinis.com>
2021-05-06 10:57:21 +0200

    fix(ember): improve profile and language switcher

--- ember/app/ui/profile/index/controller.js
+++ ember/app/ui/profile/index/controller.js
@@ -2,9 +2,24 @@ import Controller from "@ember/controller";
 import { action } from "@ember/object";
 import { tracked } from "@glimmer/tracking";
 import { lastValue, restartableTask } from "ember-concurrency-decorators";
+import QueryParams from "ember-parachute";
 
-export default class ProfileIndexController extends Controller {
-  queryParams = ["activeTab"];
+const queryParams = new QueryParams({
+  // Convert the tab number to a string so its human readable
+  activeTab: {
+    defaultValue: "identity",
+    serialize(value) {
+      return value ? "memberships" : "identity";
+    },
+    deserialize(value) {
+      return value === "identity" ? 0 : 1;
+    },
+  },
+});
+
+export default class ProfileIndexController extends Controller.extend(
+  queryParams.Mixin
+) {
   @tracked activeTab = 0;
 
   @lastValue("fetchMemberships") memberships;
