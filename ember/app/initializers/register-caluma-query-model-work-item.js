b5e8ac16f4119821131dfbf75c2163309c55ba14
yelinz <yelin.zhang@adfinis.com>
2021-09-08 15:37:35 +0200

    fix(ember): sort forms by name

--- ember/app/initializers/register-caluma-query-model-work-item.js
+++ ember/app/initializers/register-caluma-query-model-work-item.js
@@ -1,6 +1,8 @@
+import CustomFormModel from "mysagw/caluma-query/models/form";
 import CustomWorkItemModel from "mysagw/caluma-query/models/work-item";
 
 export function initialize(application) {
+  application.register("caluma-query-model:form", CustomFormModel);
   application.register("caluma-query-model:work-item", CustomWorkItemModel);
 }
 
