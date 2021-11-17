fa170c54ae8712752bb049e618eefd3c017b434d
yelinz <yelin.zhang@adfinis.com>
2021-11-17 11:44:32 +0100

    fix(ember): display identity of invitation

--- ember/app/abilities/case.js
+++ ember/app/abilities/case.js
@@ -4,10 +4,24 @@ export default class CaseAbility extends BaseAbility {
   get canExportForAccounting() {
     return this.isStaff;
   }
+
   get canList() {
-    return this.isInvited(this.model) || this.isStaff;
+    return this.hasAccess(this.model) || this.isStaff;
   }
-  get canEdit() {
-    return this.isInvited(this.model);
+
+  get canAdd() {
+    return this.hasAccess(this.model);
+  }
+
+  get canDelete() {
+    return (
+      (this.invitation.email ||
+        this.model.invitations.filter((invitation) => {
+          return (
+            !invitation.email && invitation.caseId === this.invitation.caseId
+          );
+        }).length > 1) &&
+      this.hasAccess(this.model)
+    );
   }
 }
