7c7e44a4c2fef3a24da68b1b5e2b0d8a45099496
yelinz <yelin.zhang@adfinis.com>
2021-12-08 12:12:12 +0100

    feat(ember): redirect to profile when no name is set

--- ember/app/ui/index/route.js
+++ ember/app/ui/index/route.js
@@ -3,8 +3,20 @@ import { inject as service } from "@ember/service";
 
 export default class IndexRoute extends Route {
   @service can;
+  @service store;
+  @service notification;
+  @service intl;
 
-  beforeModel() {
+  model() {
+    return this.store.queryRecord("identity", {});
+  }
+
+  afterModel(model) {
+    if (!(model.firstName || model.lastName)) {
+      this.notification.warning(this.intl.t("profile.noNameSet"));
+      return this.transitionTo("profile");
+    }
+
     if (this.can.can("list work-item")) {
       return this.transitionTo("work-items");
     }
