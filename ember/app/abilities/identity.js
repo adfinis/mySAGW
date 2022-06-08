a6bb8fb0e287897022517e1a5e9a2e76915300e0
yelinz <yelin.zhang@adfinis.com>
2022-06-08 16:33:04 +0200

    fix(ember): allow org admins to edit org through profile

--- ember/app/abilities/identity.js
+++ ember/app/abilities/identity.js
@@ -4,20 +4,24 @@ export default class IdentityAbility extends BaseAbility {
   get canList() {
     return this.isStaff;
   }
+
   get canAdd() {
     return this.isStaff;
   }
-  get canEditOrganisation() {
-    return this.isStaff;
-  }
-  canAddInterest() {
-    return this.isStaffOrOwnIdentity(this.model);
-  }
-  canRemoveInterest() {
+
+  canEdit() {
     return this.isStaffOrOwnIdentity(this.model);
   }
 
-  canEdit() {
+  get canEditOrganisation() {
+    return this.isStaff || this.model;
+  }
+
+  canAddInterest() {
+    return this.isStaffOrOwnIdentity(this.model);
+  }
+
+  canRemoveInterest() {
     return this.isStaffOrOwnIdentity(this.model);
   }
 }
