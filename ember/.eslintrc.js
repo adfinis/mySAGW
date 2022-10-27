bbde3d10c38ccc23fdcb18d104ff1bb91e659302
yelinz <yelin.zhang@adfinis.com>
2022-10-27 10:48:38 +0200

    fix(ember): run codemods for 4.0

--- ember/.eslintrc.js
+++ ember/.eslintrc.js
@@ -8,49 +8,43 @@ module.exports = {
   rules: {
     "ember/no-mixins": "warn",
   },
-  plugins: ['ember'],
-  extends: [
-    'eslint:recommended',
-    'plugin:ember/recommended',
-    'plugin:prettier/recommended',
-  ],
+  plugins: ["ember"],
   env: {
     browser: true,
   },
-  rules: {},
   overrides: [
     // node files
     {
       files: [
-        './.eslintrc.js',
-        './.prettierrc.js',
-        './.template-lintrc.js',
-        './ember-cli-build.js',
-        './testem.js',
-        './blueprints/*/index.js',
-        './config/**/*.js',
-        './lib/*/index.js',
-        './server/**/*.js',
+        "./.eslintrc.js",
+        "./.prettierrc.js",
+        "./.template-lintrc.js",
+        "./ember-cli-build.js",
+        "./testem.js",
+        "./blueprints/*/index.js",
+        "./config/**/*.js",
+        "./lib/*/index.js",
+        "./server/**/*.js",
       ],
       parserOptions: {
-        sourceType: 'script',
+        sourceType: "script",
       },
       env: {
         browser: false,
         node: true,
       },
-      plugins: ['node'],
-      extends: ['plugin:node/recommended'],
+      plugins: ["node"],
+      extends: ["plugin:node/recommended"],
       rules: {
         // this can be removed once the following is fixed
         // https://github.com/mysticatea/eslint-plugin-node/issues/77
-        'node/no-unpublished-require': 'off',
+        "node/no-unpublished-require": "off",
       },
     },
     {
       // test files
-      files: ['tests/**/*-test.{js,ts}'],
-      extends: ['plugin:qunit/recommended'],
+      files: ["tests/**/*-test.{js,ts}"],
+      extends: ["plugin:qunit/recommended"],
     },
   ],
 };
