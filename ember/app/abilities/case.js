38127e0fcd447a08dc8a9cd9ae41c1d82e9620b2
Jonas Cosandey <jonas.cosandey@adfinis.com>
2025-11-11 17:01:26 +0100

    refactor(ember): simplify redo logic

--- ember/app/abilities/case.js
+++ ember/app/abilities/case.js
@@ -1,5 +1,4 @@
 import BaseAbility from "mysagw/abilities/-base";
-import ENV from "mysagw/config/environment";
 
 export default class CaseAbility extends BaseAbility {
   get canExportForAccounting() {
@@ -34,20 +33,31 @@ export default class CaseAbility extends BaseAbility {
   }
 
   get canRedo() {
-    /*
-     * Only when additional-data-form isnt present can define-amount be redone,
-     * as it means the additional-data step was skipped.
-     * Otherwise canRedoWorkItem returns the work item which can be redone.
-     */
-    return (
-      ENV.APP.caluma.redoableTaskSlugs.includes(
-        this.model.redoWorkItem?.task.slug,
-      ) &&
-      this.isStaffOrAdmin &&
-      (this.model.canRedoWorkItem ||
-        (!this.model.workItems.findBy("task.slug", "additional-data-form") &&
-          this.model.workItems[0].task.slug === "define-amount"))
+    const canRedoTaskSlug = [
+      "circulation",
+      "decision-and-credit",
+      "additional-data",
+      "complete-document",
+    ];
+
+    // Only when additional-data-form isnt present can define-amount be redone,
+    // as it means the additional-data step was skipped.
+    // This is handled in the backend by the dynamic task redo-define-amount.
+    const additionalDataFormWorkItem = this.model.workItems.find(
+      (wi) => wi.task.slug === "additional-data-form",
     );
+    const defineAmountWorkItem = this.model.readyWorkItems.find(
+      (wi) => wi.task.slug === "define-amount",
+    );
+    if (!additionalDataFormWorkItem && defineAmountWorkItem) {
+      canRedoTaskSlug.push("define-amount");
+    }
+
+    const canRedo = this.model.readyWorkItems.some((workItem) =>
+      canRedoTaskSlug.includes(workItem.task.slug),
+    );
+
+    return this.isStaffOrAdmin && canRedo;
   }
 
   get canReopen() {
