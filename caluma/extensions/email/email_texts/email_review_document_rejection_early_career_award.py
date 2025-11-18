73794fb5d66065571412372064d53e01a6bca685
Jonas Cosandey <jonas.cosandey@adfinis.com>
2025-11-18 14:04:25 +0100

    feat(work_item_mails): send eca specific mails

---
+++ caluma/extensions/email/email_texts/email_review_document_rejection_early_career_award.py
@@ -0,0 +1,23 @@
+SUBJECT = "Early Career Award: Incomplete application"
+BODY = """Dear {first_name} {last_name}
+
+To complete the verification of your application’s formal eligibility for our Early Career Award, we require additional information. We kindly ask you to submit the missing information as soon as possible.
+
+Reference: {dossier_nr}
+
+You can access your online application form via the following link, where you will find details of the information that is still required:
+{link}
+
+If you have any questions, please contact us at sagw@sagw.ch or by phone at +41 31 306 92 50.
+
+Yours sincerely,
+
+The SAGW Team
+
+******
+
+This is an automatically generated message. Please do not reply. The contact details of the Swiss Academy of Humanities and Social Sciences can be found on the following website: www.sagw.ch/kontakt-mysagw
+"""
+
+EMAIL_SUBJECTS = {"de": SUBJECT, "fr": SUBJECT, "en": SUBJECT}
+EMAIL_BODIES = {"de": BODY, "fr": BODY, "en": BODY}
